4e2bb118b3b60ca9b14cd8e224ae097b3995f93e
Yuki-Kanai <yuki-kanai550@g.ecc.u-tokyo.ac.jp>
2018-03-30 11:04:54 +0900

    fix EOF

--- pi_diff_threads.py
+++ pi_diff_threads.py
@@ -3,7 +3,7 @@ import subprocess
 if __name__ == '__main__':
     for i in range(1,8):
         thread_set_file = open('host','w')
-        thread_set_file.write('75.pi slots=' + str(i//2) +'\n76.pi slots='+ str(i-i//2))
+        thread_set_file.write('75.pi slots=' + str(i//2) +'\n76.pi slots='+ str(i-i//2)+'\n')
         thread_set_file.close()
         string = subprocess.Popen('(time mpirun -np 1 -hostfile host ./a.out 1000000 ' + str(i) + ' 2>> ./log.data', stderr=subprocess.PIPE, shell=True).communicate()[1]
      
