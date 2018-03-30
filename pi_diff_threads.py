e57a1422fa0b0f6c6134ac11eaed02b43ce05210
Yuki-Kanai <yuki-kanai550@g.ecc.u-tokyo.ac.jp>
2018-03-30 11:10:58 +0900

    fix bash error

--- pi_diff_threads.py
+++ pi_diff_threads.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
         thread_set_file = open('host','w')
         thread_set_file.write('75.pi slots=' + str(i//2) +'\n76.pi slots='+ str(i-i//2)+'\n')
         thread_set_file.close()
-        string = subprocess.Popen('(time mpirun -np 1 -hostfile host ./a.out 1000000 ' + str(i) + ' 2>> ./log.data', stderr=subprocess.PIPE, shell=True).communicate()[1]
+        subprocess.call('(time mpirun -np 1 -hostfile host ./a.out 1000000000 ' + str(i) + ') 22>> ./log.data', shell=True)
      
